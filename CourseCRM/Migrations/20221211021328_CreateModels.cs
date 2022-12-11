1ff570af16df6822b68bf7bb14fcccc761b5950b
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-10 23:35:02 -0300

    feat: Criação das Migrations

---
+++ CourseCRM/Migrations/20221211021328_CreateModels.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseCRM.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enrollment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    LeadId = table.Column<int>(type: "int", nullable: false),
+                    Registration = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrollment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Enrollment_Course_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Course",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Leads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    EnrollmentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leads", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Leads_Enrollment_EnrollmentId",
+                        column: x => x.EnrollmentId,
+                        principalTable: "Enrollment",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollment_CourseId",
+                table: "Enrollment",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Leads_EnrollmentId",
+                table: "Leads",
+                column: "EnrollmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Leads");
+
+            migrationBuilder.DropTable(
+                name: "Enrollment");
+
+            migrationBuilder.DropTable(
+                name: "Course");
+        }
+    }
+}
---
+++ CourseCRM/Migrations/20221211022011_UpdateModelEnrollment.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseCRM.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModelEnrollment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Leads_Enrollment_EnrollmentId",
+                table: "Leads");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Leads_EnrollmentId",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "EnrollmentId",
+                table: "Leads");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollment_LeadId",
+                table: "Enrollment",
+                column: "LeadId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Leads_LeadId",
+                table: "Enrollment",
+                column: "LeadId",
+                principalTable: "Leads",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Leads_LeadId",
+                table: "Enrollment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Enrollment_LeadId",
+                table: "Enrollment");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EnrollmentId",
+                table: "Leads",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Leads_EnrollmentId",
+                table: "Leads",
+                column: "EnrollmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Leads_Enrollment_EnrollmentId",
+                table: "Leads",
+                column: "EnrollmentId",
+                principalTable: "Enrollment",
+                principalColumn: "Id");
+        }
+    }
+}
