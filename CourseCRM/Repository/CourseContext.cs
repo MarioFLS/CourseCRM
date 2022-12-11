cb72ba151f7b695f9d6dec5f9f1a87c90d31b402
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-10 23:35:13 -0300

    feat: Criação do contexto

---
+++ CourseCRM/Repository/CourseContext.cs
@@ -0,0 +1,25 @@
+﻿using CourseCRM.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseCRM.Repository
+{
+    public class CourseContext : DbContext
+    {
+        public CourseContext(){}
+        public CourseContext(DbContextOptions<CourseContext> options) : base(options){}
+
+        public DbSet<Lead> Leads { get; set; }
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Enrollment> Enrollment { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=127.0.0.1;Database=course;User=SA;Password=Password12!;Encrypt=False");
+
+            }
+        }
+
+    }
+}
