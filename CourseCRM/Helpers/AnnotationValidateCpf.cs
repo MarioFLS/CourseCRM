cf3430e8efd5f6620dfdd11c406ff0be230a67ba
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-11 19:22:56 -0300

    feat: Validação de CPF

---
+++ CourseCRM/Helpers/AnnotationValidateCpf.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace CourseCRM.Helpers
+{
+    // Validação de CPF https://pt.stackoverflow.com/questions/7910/como-validar-cpf-com-dataannotation-no-cliente-e-servidor
+    public class AnnotationValidateCpf : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+                return true;
+
+            bool valido = ValidateCpf.IsCpf(value.ToString()!);
+            return valido;
+        }
+    }
+}
