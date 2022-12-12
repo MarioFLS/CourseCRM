2ce270f9687884cb6fe216bf51c9613a89897567
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-12 20:54:20 -0300

    perf: Exclusão dá rota de criar Lead na controller Home

--- CourseCRM/Controllers/HomeController.cs
+++ CourseCRM/Controllers/HomeController.cs
@@ -20,36 +20,6 @@ namespace CourseCRM.Controllers
             return View(leads);
         }
 
-        public IActionResult CreateLeader()
-        {
-            return View("~/Views/Lead/CreateLeader.cshtml");
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult CreateLeader(Lead lead)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-
-                    lead = _courseRepository.CreateLeader(lead);
-
-                    TempData["MensagemSucesso"] = "Usuário cadastrado com sucesso!";
-                    return RedirectToAction("Index");
-                }
-
-                return View("~/Views/Lead/CreateLeader.cshtml", lead);
-            }
-            catch (Exception)
-            {
-                TempData["MensagemErro"] = $"Não foi possível cadastrar seu usuário, tente novamante!" +
-                    $"\nProvavelmente já existe um estudante com esse Email ou CPF!";
-                return View("~/Views/Lead/CreateLeader.cshtml", lead);
-            }
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
