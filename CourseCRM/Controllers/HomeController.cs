21343a5e02cd0028a75887836f4120d49550ccf7
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-11 21:06:28 -0300

    feat: Rota para Adicionar novos Estudantes

--- CourseCRM/Controllers/HomeController.cs
+++ CourseCRM/Controllers/HomeController.cs
@@ -20,9 +20,39 @@ namespace CourseCRM.Controllers
             return View(leads);
         }
 
-        public IActionResult Privacy()
+        public IActionResult CreateLeader()
         {
-            return View();
+            return View("~/Views/Lead/CreateLeader.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateLeader(Lead lead)
+        {
+            try
+            {
+                var getLead = _courseRepository.GetLead(lead.Email, lead.Cpf);
+                if (getLead != null)
+                {
+                    throw new Exception("Já existe um estudante com esse Email ou CPF!");
+                }
+                if (ModelState.IsValid)
+                {
+
+                    lead = _courseRepository.CreateLeader(lead);
+
+                    TempData["MensagemSucesso"] = "Usuário cadastrado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+
+                return View("~/Views/Lead/CreateLeader.cshtml", lead);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Não foi possível cadastrar seu usuário, tente novamante!" +
+                    $"\nDetalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
