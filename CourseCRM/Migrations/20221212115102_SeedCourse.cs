9b41b8bbaf1b60afd9e53868efe3d791990f94d3
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-12 09:05:28 -0300

    feat: seed do Course

---
+++ CourseCRM/Migrations/20221212115102_SeedCourse.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CourseCRM.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCourse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Course",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "C#" },
+                    { 2, "dotNet" },
+                    { 3, "JavaScript" },
+                    { 4, "React" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
